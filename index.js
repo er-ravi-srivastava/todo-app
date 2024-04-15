eeb24909b0f8468591a9b6703ff4361b1feb036a
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2024-04-15 11:31:04 +0530

    updates

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ if(!parsedPayload.success) {
     await todo.create({
         title: createPayload.title,
         description: createPayload.description,
+        completed:false
     })
 
     res.json({
@@ -32,12 +33,34 @@ if(!parsedPayload.success) {
 
 })
 
-app.post("/todos",function(req,res){
+app.post("/todos", async function(req,res){
 
-const 
-
-})
-
-app.put("/completed",function(req,res){
+    const todo = await todo.find({});
+    console.log(todos) // promise
 
 })
+
+app.put("/completed"), async function(req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    
+    if (!parsedPayload.success) {
+        // Status code 400 (Bad Request) is more appropriate for input validation errors
+        return res.status(400).json({
+            msg: "You sent the wrong inputs"
+        })
+
+        return;
+    }
+
+    await todo.update({
+        _id: req.body.id
+
+    },{
+        completed: true
+    })
+    res.json({
+        msg: "Todo marked as completed"
+    })
+
+};
