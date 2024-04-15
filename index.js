31f49436251dc5f892610a5f64719c9e25ce8101
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2024-04-15 11:11:06 +0530

    Updates

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require("express");
+const { createTodo } = require("./types");
 const app = express();
+const { todo } = require("./db");
+
+
 
 
 app.use(express.json());
 
 
-app.post("/todo",function(req,res){
+app.post("/todo",async function(req,res){
+const createPayload = req.body;
+const parsedPayload = createTodo.safeParse(createPayload);
+if(!parsedPayload.success) {
+    res.status(411).json({
+        msg: "You sent the wrong inputs",
+    })
+    return;
+}
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+    })
+
+    res.json({
+        msg: "todo created"
+    })
+
+
 
 })
 
-
 app.post("/todos",function(req,res){
+
+const 
 
 })
 
